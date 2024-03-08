6bf52fa91bb23a1a2984c08688f5375af95676a8
acklongwe@gmail.com <acklongwe@gmail.com>
2024-03-09 00:57:11 +0200

    Add create iterator object

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,18 @@
+export default function createIteratorObject(report) {
+  const { allEmployees } = report;
+
+  function* iterateEmployees() {
+    for (const department in allEmployees) {
+      if (Object.prototype.hasOwnProperty.call(allEmployees, department)) {
+        const employees = allEmployees[department];
+        for (const employee of employees) {
+          yield employee;
+        }
+      }
+    }
+  }
+
+  return {
+    [Symbol.iterator]: iterateEmployees,
+  };
+}
