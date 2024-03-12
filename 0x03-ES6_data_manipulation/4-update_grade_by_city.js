5b5c76140829047f79ac0e5c02e355b24a03c244
acklongwe@gmail.com <acklongwe@gmail.com>
2024-03-12 16:10:04 +0200

    Add filter student grades by city

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,12 @@
-export default function updateStudentGradeByCity(studentList, city, newGrade) {
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
   return studentList.map((student) => {
-    if (student.city === city) {
-      return { ...student, grade: newGrade };
+    if (student.location === city) {
+      const updatedGrade = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
+      if (updatedGrade !== undefined) {
+        return { ...student, grade: updatedGrade.grade };
+      }
+      return { ...student, grade: 'N/A' };
     }
     return student;
-  });
+  }).filter((student) => student.location === city);
 }
