604a338ecfc4f468f10a797ce87aaa152443e0ef
AckimJnr <acklongwe@gmail.com>
2024-04-30 23:39:26 +0200

    Add read file

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.trim().split('\n');
+
+    // Remove empty lines
+    const nonEmptyLines = lines.filter((line) => line.trim() !== '');
+
+    // Parse CSV data
+    const students = nonEmptyLines.map((line) => line.split(','));
+
+    // Count students in each field
+    const fields = {};
+    students.forEach((student) => {
+      const field = student[3];
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(student[0]);
+    });
+
+    console.log('Number of students:', nonEmptyLines.length);
+    for (const field in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        const studentCount = fields[field].length;
+        const studentList = fields[field].join(', ');
+        console.log(`Number of students in ${field}: ${studentCount}. List: ${studentList}`);
+      }
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
