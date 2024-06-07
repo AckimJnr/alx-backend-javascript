e35a9414beebfe3637fc5abf02eb4deb3de33eaf
Ackim Longwe <116080565+AckimJnr@users.noreply.github.com>
2024-06-07 15:32:00 +0200

    Add payment token test file

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const expect = chai.expect;
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function() {
+  it('should resolve with the correct data when success is true', function(done) {
+    getPaymentTokenFromAPI(true)
+      .then(response => {
+        expect(response).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch(error => {
+        done(error);
+      });
+  });
+});
