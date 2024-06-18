2db19e7f386eeb40d5417bdf0aab3bb8e86e7341
Ackim Longwe <116080565+AckimJnr@users.noreply.github.com>
2024-06-18 19:52:18 +0200

    Update input output

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,6 +3,7 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+  terminal: false,
 });
 
 console.log('Welcome to Holberton School, what is your name?');
