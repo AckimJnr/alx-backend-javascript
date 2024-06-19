e7ba1d6974c95f6eba045c9465a1c2b28777fde1
Ackim Longwe <116080565+AckimJnr@users.noreply.github.com>
2024-06-19 09:00:34 +0200

    Update stdin file

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,17 +6,14 @@ const rl = readline.createInterface({
   terminal: false,
 });
 
-console.log('Welcome to Holberton School, what is your name?');
+console.log('Welcome to Holberton School, what is your name?\n');
 
 rl.question('', (name) => {
-  console.log(`Your name is: ${name.trim()}\r`);  // Append \r to match the expected output format
+  console.log(`Your name is: ${name.trim()}`);
 
   if (!process.stdin.isTTY) {
     console.log('This important software is now closing');
   }
 
   rl.close();
-
-  // Delay exit to ensure all output is written before the process exits
-  setTimeout(() => process.exit(), 100);
 });
