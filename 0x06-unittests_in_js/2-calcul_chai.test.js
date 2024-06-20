9bb9da3957887d549c0971a5bee0c865d98a7800
Ackim Longwe <116080565+AckimJnr@users.noreply.github.com>
2024-06-20 13:28:25 +0200

    Update basic testing using chai

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,20 +1,21 @@
-const assert = require('assert');
-const calculateNumber = require('./1-calcul');
+const expect = require('chai').expect;
+const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
-  it('should return 6 when inputs are 1.4 and 4.5', () => {
-    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
-  });
+    it('should return 6 when inputs are 1.4 and 4.5', () => {
+        expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    });
 
-  it('should return -4 when inputs are 1.4 and 4.5', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-  });
+    it('should return -4 when inputs are 1.4 and 4.5', () => {
+        expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    });
 
-  it('should return 0.2 when inputs are 1.4 and 4.5', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-  });
+    it('should return 0.2 when inputs are 1.4 and 4.5', () => {
+        expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
 
-  it('should return Error when inputs are 1.4 and 0', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-  });
+    it('should return Error when inputs are 1.4 and 0', () => {
+        expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
 });
+
